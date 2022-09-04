ff7d44f707c7c66b301550221a04906d372f2e26
SulemanMughal <sulemanmughal155@gmail.com>
2022-09-04 15:56:13 +0500

    Commen Feature
    Added

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -15,4 +15,11 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ('status', 'publish')
 
 
-admin.register(Comment)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+admin.site.register(Comment, CommentAdmin)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,12 @@
-from .models import Post
-from .forms import EmailPostForm
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+
+# import models
+from .models import Post, Comment
+
+# import forms
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 
@@ -33,9 +37,25 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     template_name = 'post/detail.html'
     context = {
-        'post' : post
+        'post' : post,
+        'comments': comments,
+        'comment_form': comment_form
     }
     return render(request, template_name, context)
 
